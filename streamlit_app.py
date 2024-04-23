2ae025a7640f0311ad407c038b3a3c0aa1457e6e
Gilbert Michael Redman <gilbertmichaelredman@gmail.com>
2024-04-23 13:47:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
+
+#New Section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
