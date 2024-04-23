22d4b814273233205071d1b4cc17e7858d53ce53
Gilbert Michael Redman <gilbertmichaelredman@gmail.com>
2024-04-23 14:21:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,10 +43,8 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-
-    st.write(my_insert_stmt)
+     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string +", "+ """','"""+name_on_order+"""')"""
 
     time_to_insert = st.button('Submit Order')
 
