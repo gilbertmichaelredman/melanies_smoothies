cdc463cc495dd3e5f9aa2350e03aea66af7b9bfd
Gilbert Michael Redman <gilbertmichaelredman@gmail.com>
2024-04-23 13:48:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ if ingredients_list:
 #New Section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
