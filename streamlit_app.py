1a26dabc3545e41f23755b0be1a703e36f5733de
Gilbert Michael Redman <gilbertmichaelredman@gmail.com>
2024-04-23 13:49:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,5 @@ if ingredients_list:
 #New Section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
