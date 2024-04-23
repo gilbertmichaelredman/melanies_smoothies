9aead2960a6fd411012c66c81b21ea560577e5b8
Gilbert Michael Redman <gilbertmichaelredman@gmail.com>
2024-04-23 14:10:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.write('The current smoothie title is', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #Convert the snowpark dataframe to pandas
 pd_df=my_dataframe.to_pandas()
