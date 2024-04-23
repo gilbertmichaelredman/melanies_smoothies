c7d1ddf7ae217fb5b02fb0f451af7ace2e8b91fc
Gilbert Michael Redman <gilbertmichaelredman@gmail.com>
2024-04-23 14:22:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+        ingredients_string += fruit_chosen + ' '
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -43,8 +43,7 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
-     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string +", "+ """','"""+name_on_order+"""')"""
+     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string +", "+ """','"""+name_on_order+"""')"""
 
     time_to_insert = st.button('Submit Order')
 
